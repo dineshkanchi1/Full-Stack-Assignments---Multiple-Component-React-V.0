ced504da1930479410e37152c720793a27ce98cf
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-23 15:01:00 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-
+import Test from "react-test-attributes";
 class App extends Component {
   render() {
     return (
       <>
-        <h1>4R-Spot</h1>
-        <p>
-          Provide a interface between the buyer and seller for recycle items
-          from scrap
-        </p>
+        <Test id="project-name">
+          <h1>4R-Spot</h1>
+        </Test>
+        <Test id="project-description">
+          <p>
+            Provide a interface between the buyer and seller for recycle items
+            from scrap
+          </p>
+        </Test>
       </>
     );
   }
