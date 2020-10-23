21a45db999432117667eee803cdb52c26e2d3ae9
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-23 16:09:28 +0530

    commit

--- parser.js
+++ parser.js
@@ -1,9 +1,9 @@
-var parser = require('xml-js');
+var parser = require("xml-js");
 
-const xml = require('fs').readFileSync('junit.xml', 'utf-8');
+const xml = require("fs").readFileSync("junit.xml", "utf-8");
 
-const options = {compact: true, ignoreComment: true, spaces: 4};
+const options = { compact: true, ignoreComment: true, spaces: 4 };
 
-const result = parser.xml2json(xml, options)
+const result = parser.xml2json(xml, options);
 
 console.log(result);
