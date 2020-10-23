673ab8a5571f8a18bb76def3edc4e8b8d8d2bb7d
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-23 16:33:18 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import ProjectDescription from "./ProjectDescription";
-import ProjectName from "./ProjectName";
 class App extends Component {
   render() {
     return (
       <>
-        <ProjectName key="project-name" data-ns-test="project-name" />
-        <ProjectDescription
-          key="project-description"
-          data-ns-test="project-description"
-        />
+        <h1 data-ns-test="project-name">4R-Spot</h1>
+        <p data-ns-test="project-description">
+          Provide a interface between the buyer and seller for recycle items
+          from scrap
+        </p>
       </>
     );
   }
--- src/components/ProjectName.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class ProjectName extends React.Component {
-  render() {
-    return <h1>4R-Spot</h1>;
-  }
-}
